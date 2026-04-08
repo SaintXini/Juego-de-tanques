c6eec4c78e406737e759dbaed8cdcaa5e652d136
Saint <ss.mscx@gmail.com>
2026-04-08 17:11:06 -0600

    diseño solamente del tanque sin incluir movimientos aún

--- js/app.js
+++ js/app.js
@@ -2,38 +2,35 @@
 VARIABLES
 ************/
 game = {
-	canvas: null,
-	ctx: null,
-	caratula: true,
-	x:0, 
-	y:0,
-	imagen: null,
-	radianes: null,
-	teclaPulsada: null,
-	tecla_array: new Array(),
-	balas_array: new Array(),
-	enemigos_array: new Array(),
-	colorEnemigo: ["red", "blue", "black","white", "yellow", "pink", "purple"],
-	colorBala: "red",
-	centroX: 0, 
-	centroY: 0,
-	w:0,
-	h:0,
-	puntos: 0,
-	vidas: 3,
-	finJuego: false,
-}
+  canvas: null,
+  ctx: null,
+  caratula: true,
+  x: 0,
+  y: 0,
+  imagen: null,
+  radianes: null,
+  teclaPulsada: null,
+  tecla_array: new Array(),
+  balas_array: new Array(),
+  enemigos_array: new Array(),
+  colorEnemigo: ["red", "blue", "black", "white", "yellow", "pink", "purple"],
+  colorBala: "red",
+  centroX: 0,
+  centroY: 0,
+  w: 0,
+  h: 0,
+  puntos: 0,
+  vidas: 3,
+  finJuego: false,
+};
 
 sonidos = {
-	boing: null,
-	disparo: null,
-	intro: null,
-	fin: null,
-	boom: null
-
-}
-
-
+  boing: null,
+  disparo: null,
+  intro: null,
+  fin: null,
+  boom: null,
+};
 
 /**************
 CONSTANTES
@@ -42,110 +39,122 @@ const BARRA = 32;
 /**************
 OBJETOS
 **************/
-function Bala(x,y,radianes){
-	this.x = x;
-	thix.y = y;
-	thiw.w = 5;
-	this.velocidad = 8;
-	this.radianes = radianes;
-	this.dibujar = function() {
-		
-	}
-	
+function Bala(x, y, radianes) {
+  this.x = x;
+  this.y = y;
+  this.w = 5;
+  this.velocidad = 8;
+  this.radianes = radianes;
+  this.dibujar = function () {};
 }
-function Tanque(x,y,radio){
-	thix.x = x;
-	thix.y = y;
-	this.radio = radio;
-	this.rotacion = 0;
-	this.escala = 1;
-	this.w = 0;
-	this.h = 0;
-	this.dibujar = function() {
-		
-	}
-}
-
-function Enemigo(x,y){
-	thix.x = x;
-	thix.y = y;
-	this.n = 0;
-	this.inicioX = x;
-	this.inicioY = y;
-	this.estado = 1;
-	this.r = 10;
-	this.w = r *2;
-	this.vive = true;
-	this.velocidad = .3+Math.random();
-	this.color = game.colorEnemigo[Math.floor(Math.random()*game.colorEnemigo.length)];
-	this.dibujar = function() {
-		
-	}
-	
+function Tanque(x, y, radio) {
+  this.x = x;
+  this.y = y;
+  this.radio = radio;
+  this.rotacion = 0;
+  this.escala = 1;
+  this.w = 0;
+  this.h = 0;
+  this.dibujar = function () {
+    game.imagen.src = "imagenes/tanque.png";
+    game.imagen.onload = function () {
+      this.w = game.imagen.width;
+      this.h = game.imagen.height;
+      let ww = this.w / 2;
+      let hh = this.h / 2;
+      game.ctx.drawImage(game.imagen, game.centroX - ww, game.centroY - hh);
+    };
+  };
 }
 
+function Enemigo(x, y) {
+  this.x = x;
+  this.y = y;
+  this.n = 0;
+  this.inicioX = x;
+  this.inicioY = y;
+  this.estado = 1;
+  this.r = 10;
+  this.w = r * 2;
+  this.vive = true;
+  this.velocidad = 0.3 + Math.random();
+  this.color =
+    game.colorEnemigo[Math.floor(Math.random() * game.colorEnemigo.length)];
+  this.dibujar = function () {};
+}
 
 /***********
 FUNCIONES
 ************/
-const caratula=()=>{
-	let imagen = new Image();
-	imagen.src = "imagenes/caratula.png";
+const caratula = () => {
+  let imagen = new Image();
+  imagen.src = "imagenes/caratula.png";
 
-	imagen.onload =()=>{
-		game.ctx.drawImage(imagen,0,0,700,500);
-	}
-}
-const seleccionar=(e)=>{
-	if(game.caratula){
-		inicio();
-	}
-}
-const inicio=()=>{
-	game.ctx.clearRect(0,0,game.canvas.width,game.canvas.height);
-	game.caratula = false;
-	//sonidos.boom.play();
-	//sonidos.disparo.play();
-	//sonidos.intro.play();
-	//sonidos.fin.play();
-	//sonidos.boing.play();
+  imagen.onload = () => {
+    game.ctx.drawImage(imagen, 0, 0, 700, 500);
+  };
+};
+const seleccionar = (e) => {
+  if (game.caratula) {
+    inicio();
+  }
+};
+const inicio = () => {
+  game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
+  game.caratula = false;
+  //sonidos.boom.play();
+  //sonidos.disparo.play();
+  //sonidos.intro.play();
+  //sonidos.fin.play();
+  //sonidos.boing.play();
 
-	animar();
-}
-const animar = () =>{
-	requestAnimationFrame(animar);
-	verificar();
-	pintar();
-}
-const verificar = () =>{
-
-} 
-const pintar=()=>{
-
-}
+  animar();
+};
+const animar = () => {
+  requestAnimationFrame(animar);
+  verificar();
+  pintar();
+};
+const verificar = () => {};
+const pintar = () => {
+  game.tanque.dibujar();
+};
 /***********
 INICIO
 ************/
-window.requestAnimationFrame=(function(){
-	return window.requestAnimationFrame || 
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame ||
-	function(callback){window.setTimeout(callback,17);}
+window.requestAnimationFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 17);
+    }
+  );
 })();
-window.onload=function(){
-	game.canvas = document.getElementById("canvas");
-	if(game.canvas && game.canvas.getContext){
-		game.ctx = canvas.getContext("2d");
-		if (game.ctx) {
-			sonidos.boing = document.getElementById("boing");
-			sonidos.disparo = document.getElementById("disparo");
-			sonidos.intro = document.getElementById("intro");
-			sonidos.boing = document.getElementById("boing");
-			sonidos.boom = document.getElementById("boom");
-			caratula();
-			game.canvas.addEventListener("click",seleccionar,false);
-		} else{
-			alert("NO cuentas con CANVAS")
-		};
-	}
-}
+window.onload = function () {
+  game.canvas = document.getElementById("canvas");
+  if (game.canvas && game.canvas.getContext) {
+    game.ctx = canvas.getContext("2d");
+    if (game.ctx) {
+      sonidos.boing = document.getElementById("boing");
+      sonidos.disparo = document.getElementById("disparo");
+      sonidos.intro = document.getElementById("intro");
+      sonidos.boing = document.getElementById("boing");
+      sonidos.boom = document.getElementById("boom");
+
+      // Configuración para que aparezca el tanquesito
+      game.w = game.canvas.width;
+      game.h = game.canvas.height;
+      game.centroX = game.w / 2;
+      game.centroY = game.h / 2;
+      game.imagen = new Image();
+      game.tanque = new Tanque(game.centroX, game.centroY);
+
+      caratula();
+      game.canvas.addEventListener("click", seleccionar, false);
+    } else {
+      alert("NO cuentas con CANVAS");
+    }
+  }
+};
